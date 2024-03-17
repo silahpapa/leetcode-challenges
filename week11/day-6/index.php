28d77c51fc228a5e97649d638668c41a589a5744
silah <silakosy@gmail.com>
2024-03-17 22:36:53 +0300

    interger to roman

---
+++ week11/day-6/index.php
@@ -0,0 +1,34 @@
+<?php
+class Solution {
+
+    /**
+     * @param Integer $num
+     * @return String
+     */
+    function intToRoman($num) {
+        $romanNumerals = array(
+            'M' => 1000,
+            'CM' => 900,
+            'D' => 500,
+            'CD' => 400,
+            'C' => 100,
+            'XC' => 90,
+            'L' => 50,
+            'XL' => 40,
+            'X' => 10,
+            'IX' => 9,
+            'V' => 5,
+            'IV' => 4,
+            'I' => 1
+        );
+
+        $result = '';
+        foreach ($romanNumerals as $roman => $value) {
+            while ($num >= $value) {
+                $result .= $roman;
+                $num -= $value;
+            }
+        }
+        return $result;
+    }
+}
